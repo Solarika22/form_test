5729550e353b2b5b0ef6f77f56bfd1e28eab5292
Solarika22 <solarikarma@gmail.com>
2021-05-24 21:01:58 +0500

    edited docs

--- src/test/java/docs/CssXpathExamples.java
+++ src/test/java/docs/CssXpathExamples.java
@@ -1,8 +1,8 @@
 package docs;
 
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.byId;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class CssXpathExamples {
 
@@ -11,12 +11,12 @@ public class CssXpathExamples {
         // group: https://t.me/xpath_help
         // hints: https://devhints.io/xpath
 
-        // <input type="email" class="inputtext login_form_input_box" name="email" id="email" data-testid="royal_email">
+        // Если есть все <input type="email" class="inputtext login_form_input_box" name="email" id="email" data-testid="royal_email">
 
         $("[data-testid=\"royal_email\"]").setValue("some@mailmail.bf");
         $(by("data-testid", "royal_email")).setValue("some@email.com");
 
-        // <input type="email" class="inputtext login_form_input_box" name="email" id="email">
+        // Если нет data-testid <input type="email" class="inputtext login_form_input_box" name="email" id="email">
 
         $("[id='email']").setValue("some@email.com");
         $("[id=\"email\"]").setValue("some@email.com");
@@ -26,6 +26,35 @@ public class CssXpathExamples {
         $("input[id=email]").setValue("some@email.com");
         $("input#email").setValue("some@email.com");
         $(by("id", "email")).setValue("some@email.com");
+
+        $x("//input[@id='email']").setValue("some@email.com");
+        $x("//*[@name='email']").setValue("some@email.com");
+        $(byXpath("//*[@name='email']")).setValue("some@email.com");
+
+        // Если нет ID <input type="email" class="inputtext login_form_input_box" name="email">
+
+        $("[name='email']").setValue("some@email.com");
+        $("input[name='email']").setValue("some@email.com");
+        $(by("name", "email")).setValue("some@email.com");
+        $(byName("email")).setValue("some@email.com");                     // <- use this
+
+        // Если нет ID <input type="email" class="inputtext login_form_input_box">
+
+        $(byClassName("login_form_input_box")).setValue("some@email.com");
+        $(".login_form_input_box").setValue("some@email.com");                    // <- use this
+        $(".inputtext.login_form_input_box").setValue("some@email.com");
+        $("input.inputtext.login_form_input_box").setValue("some@email.com");
+        $("input.inputtext.login_form_input_box[type='email']").setValue("some@email.com");
+
+        $x("//*[@class='login_form_input_box']").setValue("some@email.com");
+
+        // <div type="email" class="inputtext">
+        //      <input class="login_form_input_box">
+        // </div>
+
+        $(".inputtext .login_form_input_box").setValue("some@email.com");
+        $(".inputtext").$(".login_form_input_box").setValue("some@email.com");
+        $(".inputtext").find(".login_form_input_box").setValue("some@email.com");
 
     }
 }
